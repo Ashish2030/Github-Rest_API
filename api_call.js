382ff44849a08ba813fd761dce9739f6e297d312
Ashish2030 <Kumarashish79924@gmail.com>
2021-04-04 04:52:16 +0530

    𝐅𝐈𝐍𝐀𝐋 𝐏𝐑𝐎𝐉𝐄𝐂𝐓

---
+++ api_call.js
@@ -0,0 +1,18 @@
+var url="https://api.github.com/users";
+var xhr= new XMLHttpRequest();
+xhr.open('GET',url);
+xhr.onreadystatechange=()=>
+{
+    if(xhr.readyState==4 && xhr.status==200)
+    {
+   
+        var m=JSON.parse(xhr.responseText);
+        console.log(typeof(xhr.responseText));
+        console.log(typeof(m));
+        //    document.querySelector('div').innerHTML=xhr.responseText;
+           document.querySelector('img').setAttribute('src',m[0].avatar_url);
+           document.querySelector('div').innerHTML=`<h3>${m[0].node_id}</h3>`; 
+
+    }
+}
+xhr.send();
