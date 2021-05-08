6acd6c391d9a08d21aa8c8012a4802d422b1811f
Ashish Kumar <kumarashish79924@gmail.com>
2021-05-08 20:12:51 +0530

    Update api_call.js

--- api_call.js
+++ api_call.js
@@ -7,9 +7,10 @@ xhr.onreadystatechange=()=>
     {
    
         var m=JSON.parse(xhr.responseText);
+        console.log(m);
         console.log(typeof(xhr.responseText));
         console.log(typeof(m));
-        console.log(xhr.responseText);
+       
            document.querySelector('img').setAttribute('src',m[0].avatar_url);
            document.querySelector('div').innerHTML=`<h3>${m[0].node_id}</h3>`; 
 
